df58aa406c238ef9274cffb9a1c52a3d03294731
mycroftjr <sonofmycroft@gmail.com>
2022-07-28 23:14:50 -0700

    human-readable configs
    the configs should now be written in a human-readable format. kill and run autoFarm.js to update the config file.

--- dist/configHelper.js
+++ dist/configHelper.js
@@ -1,28 +1,24 @@
 
 /** @param {NS} ns */
 export async function parseConfig(ns, filename, struct) {
-    if (!ns.fileExists(filename)) {
-        await ns.write(filename, JSON.stringify(struct), "w");
-    }
-    const json = JSON.parse(ns.read(filename), (k, value) => {
-        if (k in struct) {
-            const key = k;
-            if (typeof value !== typeof struct[key]) {
-                throw Error(`Type of Config value "${k}" should be "${typeof struct[key]}" (was "${typeof value}")!`);
+    let json = struct;
+    if (ns.fileExists(filename)) {
+        json = JSON.parse(ns.read(filename), (k, value) => {
+            if (k in struct) {
+                const key = k;
+                if (typeof value !== typeof struct[key]) {
+                    throw Error(`Type of Config value "${k}" should be "${typeof struct[key]}" (was "${typeof value}")!`);
+                }
             }
+            return value;
+        });
+        for (const k of Object.keys(struct)) {
+            if (k in json)
+                continue;
+            const key = k;
+            json[key] = struct[key];
         }
-        return value;
-    });
-    let anyChange = false;
-    for (const k of Object.keys(struct)) {
-        if (k in json)
-            continue;
-        const key = k;
-        json[key] = struct[key];
-        anyChange = true;
     }
-    if (anyChange) {
-        await ns.write(filename, JSON.stringify(json), "w");
-    }
+    await ns.write(filename, JSON.stringify(json, null, "\t"), "w");
     return json;
 }
